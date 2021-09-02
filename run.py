fe7904413972b53bbf75110229e68f189278afdb
Oliver-RC <oli_cummings9@hotmail.com>
2021-09-02 12:25:30 +0000

    create the minesweeper board using list of lists and removing the string

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def game(n):
+    """
+    create the minesweeper board size
+    """
+    board = [[0 for row in range(n)] for column in range(n)]
+    for row in board:
+        print(" ".join(str(cell) for cell in row))
+        print("")
+
+if __name__ == "__main__":
+    """identifies this file as the main module"""
+    game(5)
